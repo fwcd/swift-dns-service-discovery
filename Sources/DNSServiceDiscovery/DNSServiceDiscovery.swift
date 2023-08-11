8fd9a7f697f49b0251b7b4918fb22c6f2c7a803a
fwcd <fwcdmail@gmail.com>
2023-08-11 14:51:10 +0200

    Stub out implementation of DNSServiceDiscovery

--- Sources/DNSServiceDiscovery/DNSServiceDiscovery.swift
+++ Sources/DNSServiceDiscovery/DNSServiceDiscovery.swift
@@ -1,1 +1,26 @@
-import CDNSSD
+import Dispatch
+import ServiceDiscovery
+
+public class DNSServiceDiscovery: ServiceDiscovery {
+    public var defaultLookupTimeout: DispatchTimeInterval {
+        // TODO
+        .seconds(16)
+    }
+
+    public func lookup(
+        _ service: DNSService,
+        deadline: DispatchTime?,
+        callback: @escaping (Result<[DNSInstance], Error>) -> Void
+    ) {
+        // TODO
+    }
+
+    public func subscribe(
+        to service: DNSService,
+        onNext nextResultHandler: @escaping (Result<[DNSInstance], Error>) -> Void,
+        onComplete completionHandler: @escaping (CompletionReason) -> Void
+    ) -> CancellationToken {
+        // TODO
+        return .init()
+    }
+}
