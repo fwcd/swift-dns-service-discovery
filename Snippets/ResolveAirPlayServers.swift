338c9f8238926c24b71d7f584f91fc960f1e4dce
fwcd <fwcdmail@gmail.com>
2023-08-14 16:20:24 +0200

    Update resolution snippet

--- Snippets/ResolveAirPlayServers.swift
+++ Snippets/ResolveAirPlayServers.swift
@@ -6,8 +6,8 @@ let sd = DNSServiceDiscovery()
 print("Resolving all AirPlay servers...")
 sd.lookup(DNSServiceQuery(type: .airplay)) { instances in
     for instance in try! instances.get() {
-        sd.lookup(DNSServiceQuery(instance)) { instance in
-            print(instance)
+        sd.lookup(DNSServiceQuery(instance)) { resolved in
+            print("Resolved \(instance.name) to \(resolved)")
         }
     }
 }
