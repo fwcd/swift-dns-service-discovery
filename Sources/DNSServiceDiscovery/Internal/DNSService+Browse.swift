c8ccf6295c5993de182b1ed4c8e9206d0ff7989c
fwcd <fwcdmail@gmail.com>
2023-08-14 04:44:22 +0200

    Return flags in browse callback

--- Sources/DNSServiceDiscovery/Internal/DNSService+Browse.swift
+++ Sources/DNSServiceDiscovery/Internal/DNSService+Browse.swift
@@ -1,13 +1,21 @@
 import CDNSSD
 
 private typealias Identifier = UnsafeMutableRawPointer
-private var browseCallbacks: [Identifier: (Result<DNSServiceInstance, Error>) -> Void] = [:]
+private var browseCallbacks: [Identifier: (Result<DNSService.BrowseInstance, Error>) -> Void] = [:]
 
 extension DNSService {
+    /// A service instance found via `DNSService.browse`.
+    struct BrowseInstance {
+        /// The found instance.
+        let instance: DNSServiceInstance
+        /// Internal flags set by the C library. The most interesting one is probably `.moreComing`.
+        let flags: Flags
+    }
+
     /// Starts browsing a service query. Note that either a call to `.setDispatchQueue` or
     /// repeated invocations of `.processResult` will be needed, otherwise the callback will
     /// never get called.
-    static func browse(query: DNSServiceQuery, browseCallback: @escaping (Result<DNSServiceInstance, Error>) -> Void) throws -> DNSService {
+    static func browse(query: DNSServiceQuery, browseCallback: @escaping (Result<BrowseInstance, Error>) -> Void) throws -> DNSService {
         var serviceRef: CDNSSD.DNSServiceRef?
         let flags: CDNSSD.DNSServiceFlags = 0
         let interfaceIndex: UInt32 = 0
@@ -19,7 +27,7 @@ extension DNSService {
         }
         browseCallbacks[identifierBox.wrappedIdentifier] = browseCallback
 
-        let callback: CDNSSD.DNSServiceBrowseReply = { (serviceRef, flags, interfaceIndex, rawError, rawName, rawType, rawDomain, identifier) in
+        let callback: CDNSSD.DNSServiceBrowseReply = { (serviceRef, rawFlags, interfaceIndex, rawError, rawName, rawType, rawDomain, identifier) in
             guard let identifier,
                   let browseCallback = browseCallbacks[identifier] else { return }
 
@@ -29,11 +37,13 @@ extension DNSService {
                 guard let serviceType = rawType.flatMap(String.init(cString:)).map(DNSServiceType.init(rawValue:)) else { throw DNSServiceError.invalidServiceType }
                 guard let domain = rawDomain.flatMap(String.init(cString:)).map(Domain.init(rawValue:)) else { throw DNSServiceError.invalidDomain }
                 guard let name = rawName.flatMap(String.init(cString:)) else { throw DNSServiceError.invalidName }
+                let flags = Flags(rawValue: rawFlags)
 
                 let query = DNSServiceQuery(type: serviceType, domain: domain)
                 let instance = DNSServiceInstance(query: query, name: name, interfaceIndex: interfaceIndex)
+                let browseInstance = BrowseInstance(instance: instance, flags: flags)
 
-                return instance
+                return browseInstance
             })
         }
 
